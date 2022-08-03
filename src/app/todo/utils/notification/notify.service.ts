28b0bcb8524bd6b45cecde196880061f9b6fbb59
Adrian Czaicki <adrianrobimuzyke@gmail.com>
2022-08-03 22:57:12 +0200

    fix timeout logic

--- src/app/todo/utils/notification/notify.service.ts
+++ src/app/todo/utils/notification/notify.service.ts
@@ -8,7 +8,6 @@ import {NotificationModule} from "./notification.module";
 export class NotifyService implements OnDestroy {
 
   viewContainerRef!: ViewContainerRef
-  timeoutRef: ReturnType<typeof setTimeout> | undefined
 
   constructor() { }
 
@@ -19,17 +18,17 @@ export class NotifyService implements OnDestroy {
   notify(type: NotificationType, message: string, duration: number = 5000) {
     if(!this.viewContainerRef) throw new Error('Notify is not in template!')
 
+
     const componentRef = this.viewContainerRef.createComponent(NotificationComponent)
     componentRef.setInput('type', type)
     componentRef.setInput('message', message)
 
-    this.timeoutRef = setTimeout(() => {
-      componentRef.destroy()
+    setTimeout(() => {
+      componentRef && componentRef.destroy()
     }, duration)
   }
 
   ngOnDestroy(): void {
-    this.viewContainerRef.clear()
-    this.timeoutRef && clearTimeout(this.timeoutRef)
+    this.viewContainerRef && this.viewContainerRef.clear()
   }
 }
